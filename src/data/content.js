9300efe04aa26e2fdd2543fd99314d26d9e13b15
tobymiles <ekwealortobechukwu22@gmail.com>
2026-07-05 11:01:26 +0100

    removed events from navbar

--- src/data/content.js
+++ src/data/content.js
@@ -19,7 +19,7 @@ export const NAV_LINKS = [
   { label: "Explore", href: "/explore" },
   { label: "Haven Academy", href: "/haven-academy" },
   { label: "Haven Tribe", href: "/haven-tribe" },
-  { label: "Events", href: "/events" },
+  // { label: "Events", href: "/events" },
   { label: "Partnerships", href: "/friends" },
   { label: "Impact", href: "/impact" },
 ];
